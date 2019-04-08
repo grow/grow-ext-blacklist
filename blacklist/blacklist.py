db963d8455b2d85b0af02ae093f98f8e64b8ee9f
Randy Merrill <randy@blinkk.com>
2019-04-08 14:08:44 -0600

    Updating how the blacklist regular exp are updated.

--- blacklist/blacklist.py
+++ blacklist/blacklist.py
@@ -19,17 +19,6 @@ class BlacklistError(Error):
 class BlacklistPostRenderHook(hooks.PostRenderHook):
     """Handle the post-render hook."""
 
-    def __init__(self, extension):
-        super(BlacklistPostRenderHook, self).__init__(extension)
-        blacklist_terms = extension.config.get('blacklist', [])
-
-        self.blacklist = []
-        for term in blacklist_terms:
-            self.blacklist.append({
-                'term': term,
-                'pattern': re.compile(term, re.IGNORECASE),
-            })
-
     def trigger(self, previous_result, doc, raw_content, *_args, **_kwargs):
         """Execute post-render modification."""
         extensions = tuple(self.extension.config.get('extensions', ['.html',]))
@@ -37,7 +26,7 @@ class BlacklistPostRenderHook(hooks.PostRenderHook):
             return previous_result
 
         content = previous_result if previous_result else raw_content
-        for term in self.blacklist:
+        for term in self.extension.blacklist:
             if term['pattern'].search(content):
                 raise BlacklistError('Blacklisted term found in {}: {}'.format(
                     doc.pod_path, term['term']))
@@ -46,6 +35,27 @@ class BlacklistPostRenderHook(hooks.PostRenderHook):
 
 class BlacklistExtension(extensions.BaseExtension):
     """Blacklist extension."""
+
+    def __init__(self, pod, config):
+        self._config = None
+        super(BlacklistExtension, self).__init__(pod, config)
+
+    @property
+    def config(self):
+        """Extension configuration."""
+        return self._config
+
+    @config.setter
+    def config(self, config):
+        """Set the extension configuration."""
+        self._config = config
+        blacklist_terms = config.get('blacklist', [])
+        self.blacklist = []
+        for term in blacklist_terms:
+            self.blacklist.append({
+                'term': term,
+                'pattern': re.compile(term, re.IGNORECASE),
+            })
 
     @property
     def available_hooks(self):
