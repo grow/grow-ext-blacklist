dd8817adf24a78ec758f112ed75b8c632c7564e6
Randy Merrill <randy@blinkk.com>
2019-10-14 17:57:20 -0600

    Adding the ability to do a warning or the blacklist.

--- blacklist/blacklist.py
+++ blacklist/blacklist.py
@@ -22,9 +22,27 @@ class BlacklistError(Error):
 class BlacklistPreDeployHook(hooks.PreDeployHook):
     """Handle the pre deplow hook."""
 
+    @staticmethod
+    def _term_list_snippets(term_list, content):
+        snippets = []
+        for term in term_list:
+            search_result = term['pattern'].search(content)
+            if search_result:
+                snippet_start = max(0, search_result.start(0) - SNIPPET_SIZE)
+                snippet_end = min(
+                    len(search_result.string), search_result.end(0) + SNIPPET_SIZE)
+                snippet = search_result.string[snippet_start:snippet_end]
+                snippets.append({
+                    'term': term['term'],
+                    'pattern': term['pattern'],
+                    'snippet': snippet,
+                })
+        return snippets
+
+
     def trigger(self, previous_result, rendered_doc, command, *_args, **_kwargs):
         """Execute pre deploy validation."""
-        if not self.extension.blacklist:
+        if not self.extension.blacklist and not self.extension.warnings:
             return previous_result
         commands = tuple(self.extension.config.get('commands', ['build', 'deploy', 'stage',]))
         extensions = tuple(self.extension.config.get('extensions', ['.html',]))
@@ -32,15 +50,21 @@ class BlacklistPreDeployHook(hooks.PreDeployHook):
             return previous_result
 
         content = rendered_doc.read()
-        for term in self.extension.blacklist:
-            search_result = term['pattern'].search(content)
-            if search_result:
-                snippet_start = max(0, search_result.start(0) - SNIPPET_SIZE)
-                snippet_end = min(
-                    len(search_result.string), search_result.end(0) + SNIPPET_SIZE)
-                snippet = search_result.string[snippet_start:snippet_end]
-                raise BlacklistError('Blacklisted term ({term}) found in {path}: \n{snippet}'.format(
-                    path=rendered_doc.path, term=term['term'], snippet=snippet))
+
+        # Test for blacklist violation.
+        results = self._term_list_snippets(self.extension.blacklist, content)
+        if results:
+            raise BlacklistError(
+                'Blacklisted term ({term}) found in {path}: \n{snippet}'.format(
+                    path=rendered_doc.path, term=results[0]['term'], snippet=results[0]['snippet']))
+
+        # Log the warnings for the warnings list.
+        results = self._term_list_snippets(self.extension.warnings, content)
+        if results:
+            for result in results:
+                self.pod.logger.warning('Blacklist warning term ({term}) found in {path}: \n{snippet}'.format(
+                    path=rendered_doc.path, term=result['term'], snippet=result['snippet']))
+
         return previous_result
 
 
@@ -50,6 +74,16 @@ class BlacklistExtension(extensions.BaseExtension):
     def __init__(self, pod, config):
         self._config = None
         super(BlacklistExtension, self).__init__(pod, config)
+
+    @staticmethod
+    def _parse_config_patterns(patterns):
+        terms = []
+        for term in patterns:
+            terms.append({
+                'term': term,
+                'pattern': re.compile(term, re.IGNORECASE),
+            })
+        return terms
 
     @property
     def config(self):
@@ -61,12 +95,9 @@ class BlacklistExtension(extensions.BaseExtension):
         """Set the extension configuration."""
         self._config = config
         blacklist_terms = config.get('blacklist', [])
-        self.blacklist = []
-        for term in blacklist_terms:
-            self.blacklist.append({
-                'term': term,
-                'pattern': re.compile(term, re.IGNORECASE),
-            })
+        self.blacklist = self._parse_config_patterns(blacklist_terms)
+        warning_terms = config.get('warning', [])
+        self.warnings = self._parse_config_patterns(warning_terms)
 
     @property
     def available_hooks(self):
